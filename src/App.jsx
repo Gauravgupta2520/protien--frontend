06c462aeda872a9934f737e39aba9ace9b690077
Gauravgupta2520 <gauravgupta2520@gmail.com>
2026-01-03 01:18:55 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home';
 import Cart from './Pages/Cart/Cart';
 import Placeorder from './Pages/Place Order/Placeorder';
-import Footer from './Components/Footer/footer';
+import Footer from './Components/Footer/Footer';
 import SignIn from './Pages/SignIN/SignIn';
 import ChatAssistant from './Components/ChatAssistant/chatAssistant';
 
